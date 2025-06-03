59e891122042e5a0572d3ff5eddf686f42b9d443
zakirjarir <zakirjarir@gmail.com>
2025-06-03 13:09:42 +0600

    Fix router history mode and add base href for GitHub Pages deployment

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import chat from "@/components/chat.vue";
 import login from "@/components/login.vue";
 
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory('/zchat/'),
     routes
 })
 
